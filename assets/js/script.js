d8db56fcaecbcb401afd9098c3ccaa494fb8e125
ditusnova <ditusnova1@gmail.com>
2023-08-13 11:31:26 +0200

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+'use strict' ;
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType,callback);
+    }
+}
+
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNav = () => navbar.classList.toggle("active");
+
+addEventOnElements(navTogglers, "click", toggleNav);
