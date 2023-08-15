095e641e785ba2e55222de76dd3b331d360f7f03
ditusnova <ditusnova1@gmail.com>
2023-08-15 09:27:03 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,16 +21,19 @@ addEventOnElements(navTogglers, "click", toggleNav);
 
 
 /*
-HEADER NAVIGATION
+HEADER ANIMATION
 */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", () =>  {
     if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
 
